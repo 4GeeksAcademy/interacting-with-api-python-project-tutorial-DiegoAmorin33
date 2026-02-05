2452676434755676ac325d79ed5c5d53ffad2938
DiegoAmorin33 <amorinoficial33@gmail.com>
2026-02-05 18:46:39 +0000

    excercisecompleted

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
-import os
-import pandas as pd
-import matplotlib.pyplot as plt
-from dotenv import load_dotenv
-
-# load the .env file variables
-load_dotenv()
+import requests
 
 
-# Spotify API credentials
+class MusicBrainzClient:
+    def __init__(self):
+        self.base_url = "https://musicbrainz.org/ws/2"
+
+    def search_artist(self, artist_name):
+        url = f"{self.base_url}/artist"
+        params = {
+            "query": artist_name,
+            "fmt": "json"
+        }
+
+        response = requests.get(url, params=params)
+
+        if response.status_code != 200:
+            print("Error:", response.status_code)
+            return []
+
+        data = response.json()
+        return data.get("artists", [])
